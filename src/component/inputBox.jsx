342c8dd38aea14d6ee9060e909a59185a08c2088
hari0082 <2430680@rts-365.dk>
2023-11-22 12:25:19 +0100

    Add style

--- src/component/inputBox.jsx
+++ src/component/inputBox.jsx
@@ -1,6 +1,68 @@
 import React, { useId, useState } from "react";
+import styled from "styled-components";
 
-import useNumberFormatter from "../hooks/useNumberformatter.js";
+const DropDownContainer = styled("div")`
+  width: 1rem;
+  margin: 0 auto;
+`;
+const DropDownHeader = styled("div")`
+  padding: 0.1em 1.5em 0.1em 1.5em;
+  /* border: 2px solid red; */
+  border-radius: 9999px;
+  font-weight: 500;
+  color: white;
+  background: black;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  margin-top: auto;
+  margin-bottom: auto;
+  text-align: center;
+`;
+
+const DropDownListContainer = styled("div")`
+  background-color: black;
+`;
+
+const DropDownList = styled("ul")`
+  position: absolute;
+  margin: 0;
+  width: auto;
+  padding-right: 4px;
+  /* border: 2px solid red; */
+  border-radius: 9999px;
+  font-weight: 500;
+  color: white;
+  box-sizing: border-box;
+  font-size: 12px;
+  z-index: 2;
+  box-sizing: border-box;
+  max-height: 100px; /* Set a maximum height for the dropdown */
+  overflow-y: auto; /* Add a scrollbar for overflow content */
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #121316; /* color of the thumb */
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #606778; /* color of the track */
+    border-radius: 4px;
+    height: 90px;
+  }
+
+  &:first-child {
+    padding-top: 0.8em;
+    background-color: black;
+  }
+`;
+const ListItem = styled("li")`
+  list-style: none;
+  margin-bottom: 0.8em;
+`;
 
 function InputBox({
   label,
@@ -67,7 +129,7 @@ function InputBox({
   };
 
   const formatAmount = (e) => {
-    const inputValue = e.target.value
+    const inputValue = e.target.value;
     const numericValue = inputValue
       .replace(/[^\d,]/g, "")
       .replace(/(,)(?=\1)|[^0-9,]/g, "");
@@ -78,7 +140,7 @@ function InputBox({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1-2">
-        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
+        <label htmlFor={id} className="inline-block mb-2 text-black/40">
           {label}
         </label>
         <input
@@ -90,8 +152,8 @@ function InputBox({
           value={amount}
           onChange={(e) => {
             const formattedValue = formatAmount(e);
-            e.target.value = formattedValue//update the displayed value
-          
+            e.target.value = formattedValue; //update the displayed value
+
             // Attempt to convert the numeric value to a number
             const parsedValue = Number(formattedValue.replace(/,/g, ""));
 
@@ -100,21 +162,21 @@ function InputBox({
               // Update the state with the parsed numeric value
               onAmountChange && onAmountChange(parsedValue);
             } else {
-           // If the conversion fails, set an empty string or the original value
-           onAmountChange && onAmountChange(""); // Reset to empty string
+              // If the conversion fails, set an empty string or the original value
+              onAmountChange && onAmountChange(""); // Reset to empty string
             }
           }}
           onClick={handleInputClick} // Handle click event to reset the value
         />
       </div>
-      <div className="w-1/2 flex flex-wrap justify-end text-right">
-        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+      <div className="flex flex-wrap justify-end w-1/2 text-right">
+        <p className="w-full mb-2 text-black/40">Currency Type</p>
         <DropDownContainer>
           <DropDownHeader onClick={toggling}>{selectedCurrency}</DropDownHeader>
           {isOpen && (
             <DropDownListContainer>
               <DropDownList
-                className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                className="px-1 py-1 bg-gray-100 rounded-lg outline-none cursor-pointer"
                 value={selectedCurrency}
                 onChange={(e) => {
                   onCurrencyChange && onCurrencyChange(e.target.value);
