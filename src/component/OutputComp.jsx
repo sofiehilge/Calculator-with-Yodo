a3a5a0a9d013583ae6a73de90b48ae56a3f3c0ae
sofiehilge <sofie.hilge@gmail.com>
2023-11-24 11:54:15 +0100

    add period and comma to output value

--- src/component/OutputComp.jsx
+++ src/component/OutputComp.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import useNumberFormatter from "../hooks/useNumberformatter";
 
 const OutputComp = ({ updatedInputValue, selectedPlan }) => {
-  /* const formatValue = (value) => {
-    return numeral(value).format("0.0");
-  };  */
 
+  const {formattedNumbers} = useNumberFormatter([
+    updatedInputValue * 0.025,
+    updatedInputValue * 0.025 *3,
+    updatedInputValue * 0.025 *6,
+    updatedInputValue * 0.05,
+    updatedInputValue * 0.05 *3,
+    updatedInputValue * 0.05 *6,
+  ])
   return (
     <div className="mb-4 text-center">
       {selectedPlan === 2.5 && (
         <div className="flex justify-between m-4">
-          <p>In 1 year {updatedInputValue * 0.025} €</p>
-          <p>In 3 years {updatedInputValue * 0.025 * 3} €</p>
-          <p>In 6 years {updatedInputValue * 0.025 * 6} €</p>
+          <p>In 1 year {formattedNumbers[0]} €</p>
+          <p>In 3 years {formattedNumbers[1]} €</p>
+          <p>In 6 years {formattedNumbers[2]} €</p>
         </div>
       )}
 
       {selectedPlan === 5 && (
         <div className="flex justify-between m-4">
-          <p>In 1 year {updatedInputValue * 0.05} €</p>
-          <p>In 3 years {updatedInputValue * 0.05 * 3} €</p>
-          <p>In 6 years {updatedInputValue * 0.05 * 6} €</p>
+          <p>In 1 year {formattedNumbers[3]} €</p>
+          <p>In 3 years {formattedNumbers[4]} €</p>
+          <p>In 6 years {formattedNumbers[5]} €</p>
         </div>
       )}
     </div>
